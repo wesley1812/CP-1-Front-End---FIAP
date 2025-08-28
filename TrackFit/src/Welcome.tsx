281e089b1af14221c8aef19c50fd8699a68fbd53
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-28 20:45:57 -0300

    feat: cria componente de boas vindas

---
+++ TrackFit/src/Welcome.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+      <h2>Bem-vindo ao TrackFit Mini!</h2>
+      <p>“O sucesso é a soma de pequenos esforços.”</p>
+      <span role="img" aria-label="músculo">💪</span>
+    </div>
+  );
+};
+
+export default Welcome;
